1698873294160952b5d10efbfa6957e824f0a727
Maxim Krestyaninov <maxipy@ya.ru>
2024-11-13 22:35:25 +0400

    feat: add getSumOfDigits

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -291,8 +291,11 @@ function getSumToN(n) {
  *   202 => 4  // (2+0+2)
  *   5   => 5  // 5
  */
-function getSumOfDigits(/* num */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(num) {
+  return num
+    .toString()
+    .split('')
+    .reduce((acc, cur) => acc + +cur, 0);
 }
 
 /**
